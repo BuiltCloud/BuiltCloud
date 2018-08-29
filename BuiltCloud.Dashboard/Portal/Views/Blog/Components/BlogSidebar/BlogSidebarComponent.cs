5ebcc53fb0e0409cb68c01083a70a26ba1062f75
Enter <350840291@qq.com>
2018-08-29 17:28:17 +0800

    改进

---
+++ BuiltCloud.Dashboard/Portal/Views/Blog/Components/BlogSidebar/BlogSidebarComponent.cs
@@ -0,0 +1,39 @@
+﻿using Built.Mongo;
+using BuiltCloud.BlogModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuiltCloud.Portal
+{
+    [ViewComponent(Name = "BlogSidebar")]
+    public class BlogSidebarComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BlogSidebarComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string name, int count)
+        {
+            switch (name)
+            {
+                case "RecentPosts":
+                    var blogRepo = _unitOfWork.GetRepository<Blog>();
+                    var result = blogRepo.FindAll(t => t.CreatedOn, 0, count, true);
+                    return View(name, result);
+
+                case "Catalogs":
+                    return View(name, _unitOfWork.GetRepository<Catalog>().FindAll(0, count));
+
+                case "Tags":
+                    return View(name, _unitOfWork.GetRepository<BlogTag>().FindAll(0, count));
+            }
+            throw new System.Exception("name not def");
+        }
+    }
+}
